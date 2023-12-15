b69aeecf78ae6d5df47417d10a64005e11736e1e
Ahmad Gad <70626969+iAhmadGad@users.noreply.github.com>
2023-12-15 18:26:30 +0300

    Create 1.cpp

---
+++ Cpp/1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <chrono>
+#include <iomanip>
+#include <utility>
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+/*
+ * 1 - Multiples of 3 or 5
+ *
+ * Problem Statement:
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * Answer: 233168
+ */
+
+int main()
+{
+	vector<int> nums;
+	for(int i = 1; i < 1000; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0) nums.push_back(i);
+	}
+	auto sum = accumulate(nums.begin(), nums.end(), decltype(nums)::value_type(0));
+	cout << sum;
+  return 0;
+}
