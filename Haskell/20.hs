0096c439191a92789336f5c939884822c9e0cd86
iAhmadGad <ahmadasaadgad@gmail.com>
2024-01-12 18:35:16 +0200

    Edit solution to problem 20 in Haskell

--- Haskell/20.hs
+++ Haskell/20.hs
@@ -7,7 +7,7 @@ factorial x = x * factorial (x - 1)
 digitsSum :: String -> Integer
 digitsSum [] = 0
 digitsSum (x:xs) =
-  (read [x] :: Integer) + digitsSum(xs)
+  (read [x] :: Integer) + digitsSum xs
 
 main :: IO ()
 main = do
